77e632a561abd18770589e262ef7f0f4e49b6df4
MGAlvaroDev <146439656+MGAlvaroDev@users.noreply.github.com>
2024-03-04 19:44:03 +0100

    Prueba 3 en MASTER tras cambios rama

--- app/src/main/java/com/amgdev/pruebagit/MainActivity.kt
+++ app/src/main/java/com/amgdev/pruebagit/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello PRUEBA 2",
+        text = "Hello PRUEBA 3",
         modifier = modifier
     )
 }
