842feaa269d0081ef0ec120364087fd22debfe83
MGAlvaroDev <146439656+MGAlvaroDev@users.noreply.github.com>
2024-03-04 19:49:08 +0100

    Prueba MERGE

--- app/src/main/java/com/amgdev/pruebagit/MainActivity.kt
+++ app/src/main/java/com/amgdev/pruebagit/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello PRUEBA 2",
+        text = "Hello PRUEBA",
         modifier = modifier
     )
 }
