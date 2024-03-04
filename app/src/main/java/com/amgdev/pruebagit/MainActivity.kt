539491a2fa6ade709c0e6a2894eac30012d35f30
MGAlvaroDev <146439656+MGAlvaroDev@users.noreply.github.com>
2024-03-04 19:43:24 +0100

    Prueba 1 en RAMA PRIMARIA

--- app/src/main/java/com/amgdev/pruebagit/MainActivity.kt
+++ app/src/main/java/com/amgdev/pruebagit/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello PRUEBA 2",
+        text = "Hello PRUEBA 1 rama primaria",
         modifier = modifier
     )
 }
